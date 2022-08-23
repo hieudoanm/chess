126e753ea1e14d73d3f80c22a6411743d6d81829
HIEU DOAN <hieumdoan@gmail.com>
2022-08-23 21:28:51 +0700

    Update monorepo

--- packages/server/webpack.config.ts
+++ packages/server/webpack.config.ts
@@ -1,14 +1,22 @@
 import path from 'path';
+import nodeExternals from 'webpack-node-externals';
 
 const mode =
   process.env.NODE_ENV == 'production' ? 'production' : 'development';
 
 export default {
   mode,
-  entry: './src/index.ts',
+  target: 'node',
+  externals: [
+    nodeExternals(),
+    nodeExternals({
+      modulesDir: path.resolve(__dirname, '../../node_modules'),
+    }),
+  ],
+  entry: path.resolve(__dirname, 'src/server.ts'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
+    filename: 'server.js',
   },
   plugins: [
     // Add your plugins here
@@ -21,13 +29,6 @@ export default {
         loader: 'ts-loader',
         exclude: ['/node_modules/'],
       },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
-      },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
   resolve: {
